d4b8c2114841c0a480a109ae598ba275469ac449
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2022-04-28 22:23:10 -0300

    ok

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 //autoload composer
-require __DIR__ .'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Dompdf\Dompdf;
 
 $dompdf = new Dompdf();
 
-$dompdf->loadHtml('<b>OLAAAA</b>');
+$dompdf->loadHtml('<h1>teste</h1>');
 
 $dompdf->render();
 
 $dompdf-> output();
+
+?>
