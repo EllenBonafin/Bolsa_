a7276233c2ac5366c7f29540bf59b7f972b78eb8
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2022-05-30 22:27:19 -0300

    indo

--- index.php
+++ index.php
@@ -7,8 +7,12 @@ use Dompdf\Options;
 
 $options = new Options();
 $options->setChroot(__DIR__);
+$options->setIsRemoteEnabled(true); //usar links de imagens e outras coisas 
 
 $dompdf = new Dompdf($options);
+
+//pagina
+$dompdf->setPaper('A4');
 
 // $dompdf->loadHtml('<h1> deu certo inferooooooooo</h1>');
 $dompdf->loadHtmlFile(__DIR__.'/arquivo.html');
@@ -16,4 +20,6 @@ $dompdf->loadHtmlFile(__DIR__.'/arquivo.html');
 $dompdf->render();
 header('Content-type: application/pdf');
 echo $dompdf->output();
+
+$dompdf->stream('ExtensaoEeventos.pdf');
 
